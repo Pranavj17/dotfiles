ada3549ec38aa16b5f726f493a86179c945fbd3b
pranav <pranav.j@scripbox.com>
2026-05-26 12:40:43 +0530

    phase-1: track ~/.config/alacritty/alacritty.toml

--- home/files.nix
+++ home/files.nix
@@ -6,5 +6,6 @@
     ".config/shell-tests/secret.sh"     = { source = ../files/shell-tests/secret.sh;       executable = true; };
     ".config/shell-tests/run.sh"        = { source = ../files/shell-tests/run.sh;          executable = true; };
     ".config/starship.toml"             .source = ../files/starship/starship.toml;
+    ".config/alacritty/alacritty.toml" .source = ../files/alacritty/alacritty.toml;
   };
 }
